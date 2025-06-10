9bea4c5940260605ac0c0e123c497a1a87f197d7
cheeseballzzzzz <kaydclashjob@gmail.com>
2025-06-10 19:03:20 +0200

    inital commit

---
+++ src/main/java/net/chizutosuto/highlandherd/HighlandHerdClientClass.java
@@ -0,0 +1,11 @@
+package net.chizutosuto.highlandherd;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HighlandHerdClientClass implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/chizutosuto/highlandherd/HighlandHerdDataGenerator.java
@@ -0,0 +1,11 @@
+package net.chizutosuto.highlandherd;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HighlandHerdDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
